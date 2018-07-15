d2ee69e09db3d05834920cc2c510743b910585e7
Akhil Raja <p.akhilraja@gmail.com>
2018-07-16 03:06:36 +0530

    Small Bug Fixes

---
+++ Assets/Scripts/NoSleep.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoSleep : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
